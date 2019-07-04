a3aa3c3075c7b212e66ca9a2e9994bee292e2e4a
Kadosa Koltai <kadosa.koltai@incquerylabs.com>
2019-07-04 11:33:10 +0200

    Added test skeleton.

---
+++ ConmTest/src/test/java/com/incquerylabs/conptest/test/ArrowEventTest.java
@@ -0,0 +1,17 @@
+package com.incquerylabs.conptest.test;
+
+import org.junit.Test;
+
+public class ArrowEventTest {
+	
+	@Test
+	public void oneToOne() {
+		//TODO
+	}
+	
+	@Test
+	public void oneToThree() {
+		//TODO
+	}
+
+}

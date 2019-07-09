60ad5712ae1fcbf228f482e01cf32c92e4539882
Kadosa Koltai <kadosa.koltai@incquerylabs.com>
2019-07-09 10:01:45 +0200

    Added auto-generated dds classes.

---
+++ Tests/OneToOneTest/src/main/java/com/incquerylabs/onetoonetest/dds/DdsSend.java
@@ -0,0 +1,21 @@
+package com.incquerylabs.onetoonetest.dds;
+
+import java.io.File;
+
+import com.incquerylabs.onetoonetest.Sender;
+
+public class DdsSend implements Sender{
+
+	@Override
+	public void send(int n, File file) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void kill() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

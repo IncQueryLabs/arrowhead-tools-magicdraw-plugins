f4e7c5b5de27e8b0f877df07f9de647655fd439f
Kadosa Koltai <kadosa.koltai@incquerylabs.com>
2019-08-08 11:34:56 +0200

    pre lunch

--- EcoreCompartmentalizer/src/main/java/com/incquerylabs/arrowhead/compartmentaizer/Wizard.java
+++ EcoreCompartmentalizer/src/main/java/com/incquerylabs/arrowhead/compartmentaizer/Wizard.java
@@ -1,16 +1,31 @@
 package com.incquerylabs.arrowhead.compartmentaizer;
 
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Set;
+
 
 public class Wizard {
-    public static void compartmentalize(Path source, Path target) throws FileNotFoundException {
+    private static final Set<String> pack = Set.of();
+    private static final Set<String> object = Set.of();
+    private static final Set<String> attribute = Set.of();
+    private static final Set<String> relation = Set.of();
+
+    public static void compartmentalize(Path source, Path target) throws FileNotFoundException, XMLStreamException {
         if(Files.isReadable(source)){
             File ecore = source.toFile();
             if(fileFormatEquals(ecore, "ecore")){
                 if(Files.isDirectory(target)) {
+                    //validation?
+                    XMLStreamReader reader = XMLInputFactory.newInstance().createXMLStreamReader(new FileReader(ecore));
+                    //TODO: here crate the compartment here, copy tool specific information to folder one layer below
+                    subCompartmentalize(reader, target);
 
                 } else {
                     if(Files.exists(target)){
@@ -30,7 +45,16 @@ public class Wizard {
             }
         }
     }
-    
+
+    private static void subCompartmentalize(XMLStreamReader reader, Path parent) throws XMLStreamException {
+        if(reader.hasNext()){
+            reader.next();
+            while (!reader.isEndElement()){
+                //TODO fé
+            }
+        }
+    }
+
     private static boolean fileFormatEquals(File file, String string){
         String[] temp = file.getName().split(".");
         return temp[temp.length-1].equals(string);

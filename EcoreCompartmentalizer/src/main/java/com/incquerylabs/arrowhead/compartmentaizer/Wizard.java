dd0df0537342907a3d08fe12d0bb6859828e3f5f
Kadosa Koltai <kadosa.koltai@incquerylabs.com>
2019-08-08 13:17:06 +0200

    Auto-generated bindings for ecore xml scheme.

--- EcoreCompartmentalizer/src/main/java/com/incquerylabs/arrowhead/compartmentaizer/Wizard.java
+++ EcoreCompartmentalizer/src/main/java/com/incquerylabs/arrowhead/compartmentaizer/Wizard.java
@@ -1,34 +1,27 @@
 package com.incquerylabs.arrowhead.compartmentaizer;
 
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Set;
 
 
 public class Wizard {
+    //TODO reformat to classes
     private static final Set<String> pack = Set.of();
     private static final Set<String> object = Set.of();
     private static final Set<String> attribute = Set.of();
     private static final Set<String> relation = Set.of();
 
-    public static void compartmentalize(Path source, Path target) throws FileNotFoundException, XMLStreamException {
-        if(Files.isReadable(source)){
+    public static void compartmentalize(Path source, Path target) throws FileNotFoundException {
+        if (Files.isReadable(source)) {
             File ecore = source.toFile();
-            if(fileFormatEquals(ecore, "ecore")){
-                if(Files.isDirectory(target)) {
-                    //validation?
-                    XMLStreamReader reader = XMLInputFactory.newInstance().createXMLStreamReader(new FileReader(ecore));
-                    //TODO: here crate the compartment here, copy tool specific information to folder one layer below
-                    subCompartmentalize(reader, target);
-
+            if (fileFormatEquals(ecore, "ecore")) {
+                if (Files.isDirectory(target)) {
+                    //TODO here comes the magic
                 } else {
-                    if(Files.exists(target)){
+                    if (Files.exists(target)) {
                         throw new FileNotFoundException("Target is a non-directory file.");
                     } else {
                         throw new FileNotFoundException("Target directory does not exist.");
@@ -38,7 +31,7 @@ public class Wizard {
                 throw new FileNotFoundException("Source file is not a .ecore file.");
             }
         } else {
-            if(!Files.exists(source)){
+            if (!Files.exists(source)) {
                 throw new FileNotFoundException("Source path does not point to a file.");
             } else {
                 throw new FileNotFoundException("Source path points to an unreadable file.");
@@ -46,17 +39,8 @@ public class Wizard {
         }
     }
 
-    private static void subCompartmentalize(XMLStreamReader reader, Path parent) throws XMLStreamException {
-        if(reader.hasNext()){
-            reader.next();
-            while (!reader.isEndElement()){
-                //TODO fé
-            }
-        }
-    }
-
-    private static boolean fileFormatEquals(File file, String string){
+    private static boolean fileFormatEquals(File file, String string) {
         String[] temp = file.getName().split(".");
-        return temp[temp.length-1].equals(string);
+        return temp[temp.length - 1].equals(string);
     }
 }
---
+++ EcoreCompartmentalizer/src/main/java/eu/arrowhead/interchange/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.arrowhead.eu/interchange")
+package eu.arrowhead.interchange;

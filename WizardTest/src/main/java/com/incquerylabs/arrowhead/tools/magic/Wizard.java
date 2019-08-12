d8547d64229ac5a4fc386044dcf70af8262ee756
Kadosa Koltai <kadosa.koltai@incquerylabs.com>
2019-08-12 13:12:49 +0200

    Unmess.

--- WizardTest/src/main/java/com/incquerylabs/arrowhead/tools/magic/Wizard.java
+++ WizardTest/src/main/java/com/incquerylabs/arrowhead/tools/magic/Wizard.java
@@ -1,9 +1,18 @@
 package com.incquerylabs.arrowhead.tools.magic;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Wizard {
 
+	public void compartmentalize(Path source, Path target) {
+		
+	}
 	
 	public static void main(String[] args) {
-		
+		Path source = Paths.get("model/testProj.ecore");
+		Path target = Paths.get("Out");
+		Wizard ted = new Wizard();
+		ted.compartmentalize(source, target);
 	}
 }

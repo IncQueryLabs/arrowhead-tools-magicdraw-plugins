d3f0ab3bf6a43b4ba48bacfefba034107b1a8fb8
Kadosa Koltai <kadosa.koltai@incquerylabs.com>
2019-08-12 17:02:57 +0200

    Eod fixes.

--- WizardTest/src/main/java/com/incquerylabs/arrowhead/tools/magic/Wizard.java
+++ WizardTest/src/main/java/com/incquerylabs/arrowhead/tools/magic/Wizard.java
@@ -13,7 +13,7 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
-import javax.xml.stream.XMLStreamException;
+
 import org.dom4j.Document;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
@@ -28,22 +28,22 @@ import org.eclipse.emf.ecore.jaxbmodel.EPackage;
 import org.eclipse.emf.ecore.jaxbmodel.ObjectFactory;
 
 public class Wizard {
-	
-	private static final Namespace ah = new Namespace("ah", "https://www.arrowhead.eu/interchange");
+
+    private static final Namespace ah = new Namespace("ah", "https://www.arrowhead.eu/interchange");
     private static final Namespace xInc = new Namespace("xi", "http://www.w3.org/2001/XInclude");
     private static final Namespace ec = new Namespace("ec", "http://www.eclipse.org/emf/2002/Ecore");
-    
-	
-	public void compartmentalize(Path source, Path target, String name) throws IOException, JAXBException {
+
+    public void compartmentalize(Path source, Path target, String name) throws IOException, JAXBException {
         if (Files.isReadable(source)) {
             File ecore = source.toFile();
             if (isEcore(ecore)) {
                 if (Files.isDirectory(target)) {
                     Path topDir = target.resolve(name);
                     Path topXml = target.resolve(name + ".xml");
-                    
+
                     //delete previous version
                     if (Files.exists(topDir)) {
+                        //noinspection ResultOfMethodCallIgnored
                         Files.walk(topDir).sorted(Comparator.reverseOrder()).map(Path::toFile).forEach(File::delete);
                     }
                     Files.createDirectories(topDir);
@@ -53,16 +53,17 @@ public class Wizard {
                     JAXBContext context = JAXBContext.newInstance(ObjectFactory.class);
                     Unmarshaller reader = context.createUnmarshaller();
                     @SuppressWarnings("unchecked")
-					JAXBElement<EPackage> elem = (JAXBElement<EPackage>) reader.unmarshal(new FileReader(source.toFile()));
-                    if(elem == null) {
-                    	System.out.println("fuck");
+                    JAXBElement<EPackage> elem = (JAXBElement<EPackage>) reader.unmarshal(new FileReader(source.toFile()));
+                    if (elem == null) {
+                        System.out.println("fuck");
                     }
+                    assert elem != null; // a valid ecore file wouldn't fail
                     EPackage ePackage = elem.getValue();
-                    
+
                     Document doc = DocumentHelper.createDocument();
                     Element root = doc.addElement(new QName("Arrowehead", ah));
                     addCommon(root);
-                    
+
                     subCompartmentalize(ePackage, topDir, root, topXml);
                     writeDocument(topXml, doc);
                 } else {
@@ -91,27 +92,27 @@ public class Wizard {
     }
 
     @SuppressWarnings("unused")
-	private void subCompartmentalize(EObject obj, Path parent, Element topParent, Path topPath) {
+    private void subCompartmentalize(EObject obj, Path parent, Element topParent, Path topPath) {
 
     }
 
     private void subCompartmentalize(EPackage ePackage, Path parent, Element topParent, Path topPath) throws IOException {
-    	String name = ePackage.getName();
+        String name = ePackage.getName();
         Path dir = parent.resolve(name);
         Files.createDirectory(dir);
         Path xml = parent.resolve(name + ".xml");
         Files.createFile(xml);
-        
+
         Element ref = topParent.addElement(new QName("include", xInc));
         ref.addAttribute("href", topPath.relativize(xml).toString());
-        
+
         Document doc = DocumentHelper.createDocument();
         Element me = doc.addElement(new QName(name, ec));
         me.addAttribute("name", name);
         me.addAttribute("nsUri", ePackage.getNsURI());
         me.addAttribute("nsPrefix", ePackage.getNsPrefix());
         Object fac = ePackage.getEFactoryInstance();
-        if(fac != null) {
+        if (fac != null) {
             me.addAttribute("eFactoryInstance", fac.toString());
         }
         addCommon(me);
@@ -132,32 +133,31 @@ public class Wizard {
     private static boolean isEcore(File file) {
         return file.getName().endsWith(".ecore");
     }
-    
-    
+
+
     private static void addCommon(Element root) {
         root.add(ah);
         root.add(xInc);
         root.add(ec);
     }
 
-	private static final OutputFormat of = OutputFormat.createPrettyPrint();
-	
+    private static final OutputFormat of = OutputFormat.createPrettyPrint();
+
     private static void writeDocument(Path target, Document document) throws IOException {
-    	XMLWriter writer = new XMLWriter(new FileWriter(target.toAbsolutePath().toFile()), of);
-    	writer.write(document);
-    	writer.flush();
-    	writer.close();
+        XMLWriter writer = new XMLWriter(new FileWriter(target.toAbsolutePath().toFile()), of);
+        writer.write(document);
+        writer.flush();
+        writer.close();
     }
-	
-	public static void main(String[] args) {
-		Path source = Paths.get("model/testProj.ecore");
-		Path target = Paths.get("Out");
-		Wizard ted = new Wizard();
-		try {
-			ted.compartmentalize(source, target, "WizTest");
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-		
-	}
+
+    public static void main(String[] args) {
+        Path source = Paths.get("model/testProj.ecore");
+        Path target = Paths.get("Out");
+        Wizard ted = new Wizard();
+        try {
+            ted.compartmentalize(source, target, "WizTest");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }

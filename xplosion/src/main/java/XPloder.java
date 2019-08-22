1f59a71b61ec4a78eee608caabbcb68e5ff0991e
Kadosa Koltai <kadosa.koltai@incquerylabs.com>
2019-08-22 17:07:15 +0200

    Slow as hell.

--- xplosion/src/main/java/XPloder.java
+++ xplosion/src/main/java/XPloder.java
@@ -2,21 +2,19 @@ import javax.xml.stream.*;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Base64;
-import java.util.Comparator;
+import java.util.*;
 
 public class XPloder {
     private BufferedReader reader;
     private int counter = 0;
     private int c;
-    Path overDir;
+    private Path overDir;
+    private int fc;
 
-    public void xPlode(Path source, Path target) throws IOException {
+    public void xPlode(Path source, Path target, String name) throws IOException {
         if (Files.isReadable(source)) {
             File sourceFile = source.toFile();
             if (Files.isDirectory(target)) {
-                String unName = sourceFile.getName();
-                String name = unName.split(".")[0];
                 Path topDir = target.resolve(name);
                 Path topXml = target.resolve(name + ".xml.iqs");
                 //delete previous version
@@ -50,15 +48,68 @@ public class XPloder {
                                 writeTroughProcessingInstructions(writer);
                                 break;
                             default:
-                                //TODO first one special
-                                //TODO write XInclude namespace
-                                inPlode(topDir, writer); //after first
+                                writer.write("<");
+                                writer.write(c);
+                                c = reader.read();
+                                while (c != ' ' && c != '\t' && c != '\n' && c != '\r' && c != '>') {
+                                    writer.write(c);
+                                    c = reader.read();
+                                }
+                                writer.write(" xmlns:xi=\"http://www.w3.org/2001/XInclude\"");
+                                writer.write(c);
+                                if (c != '>') {
+                                    do {
+                                        c = reader.read();
+                                        writer.write(c);
+                                    } while (c != '>');
+                                }
+                                int b = c;
+                                c = reader.read();
+                                boolean me = true;
+                                while (me) {
+                                    if (c == '>') {
+                                        if (b == '/') {
+                                            writer.write(c);
+                                            me = false;
+                                        }
+                                    } else if (c == '<') {
+                                        b = c;
+                                        c = reader.read();
+                                        switch (c) {
+                                            case '!':
+                                                c = reader.read();
+                                                if (c == '-') {
+                                                    writeTroughComment(writer);
+                                                } else {
+                                                    writeTroughCdata(writer);
+                                                }
+                                                break;
+                                            case '?':
+                                                writeTroughProcessingInstructions(writer);
+                                                break;
+                                            case '/':
+                                                writeTroughEndTag(writer);
+                                                me = false;
+                                                break;
+                                            default:
+                                                inPlode(topDir, writer);
+                                        }
+                                    } else {
+                                        writer.write(c);
+                                    }
+                                    if (me) {
+                                        b = c;
+                                        c = reader.read();
+                                    }
+                                }
                         }
                     } else {
                         writer.write(c);
                     }
                     c = reader.read();
                 }
+                writer.flush();
+                writer.close();
             } else {
                 if (Files.exists(target)) {
                     throw new FileNotFoundException("Target is a non-directory file.");
@@ -76,21 +127,24 @@ public class XPloder {
     }
 
     private void inPlode(Path parent, FileWriter backWriter) throws IOException {
-        String name = Base64.getUrlEncoder().encodeToString(String.valueOf(counter++).getBytes());
+        String name = getName();
         Path xml = parent.resolve(name + ".xml");
         Path dir = parent.resolve(name);
         backWriter.write("<xi:include href=\"" + overDir.relativize(xml).toString() + "\"/>");
+        if(!Files.exists(xml)){
+            Files.createFile(xml);
+        }
         FileWriter writer = new FileWriter(xml.toFile());
         writer.write("<");
         writer.write(c);
         int b = c;
         c = reader.read();
         boolean me = true;
-        while (true) {
+        while (me) {
             if (c == '>') {
+                writer.write(c);
                 if (b == '/') {
-                    writer.write(c);
-                    return;
+                    me = false;
                 }
             } else if (c == '<') {
                 b = c;
@@ -109,6 +163,8 @@ public class XPloder {
                         break;
                     case '/':
                         writeTroughEndTag(writer);
+                        me = false;
+                        break;
                     default:
                         if (!Files.exists(dir)) {
                             Files.createDirectory(dir);
@@ -118,9 +174,13 @@ public class XPloder {
             } else {
                 writer.write(c);
             }
-            b = c;
-            c = reader.read();
+            if (me) {
+                b = c;
+                c = reader.read();
+            }
         }
+        writer.flush();
+        writer.close();
     }
 
     private void writeTroughEndTag(FileWriter writer) throws IOException {
@@ -163,5 +223,33 @@ public class XPloder {
             c = reader.read();
             writer.write(c);
         } while (!(b == '?' && c == '>'));
+    }
+
+    String[] dec = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "-", "_"};
+    Set<String> forbidden = new HashSet<>(Arrays.asList("con prn aux nul com1 com2 com3 com4 com5 com6 com7 com8 com9 com0 lpt1 lpt2 lpt3 lpt4 lpt5 lpt6 lpt7 lpt8 lpt9 lpt0".split(" ")));
+
+    private String getName() {
+        int y = counter;
+        List<String> temp = new ArrayList<>();
+        while (counter >= 0) {
+            int rem = counter % 64;
+            counter = (counter - rem)/64;
+            temp.add(dec[rem]);
+            if (counter == 0) {
+                counter = -1;
+            }
+        }
+        counter = y + 1;
+        StringBuilder b = new StringBuilder();
+        for (int i = temp.size() - 1; i >= 0; --i) {
+            b.append(temp.get(i));
+        }
+        System.out.println(counter);
+        String name = b.toString();
+        if(forbidden.contains(name.toLowerCase())){
+            name = "+" + fc++;
+        }
+        return name;
     }
 }
--- xplosion/src/test/java/Test.java
+++ xplosion/src/test/java/Test.java
@@ -3,11 +3,11 @@ import java.nio.file.Paths;
 
 public class Test {
     public static void main(String[] args) {
-        Path from = Paths.get("fr");
-        Path to = Paths.get("to");
+        Path from = Paths.get("TMT.mdxml");
+        Path to = Paths.get("Out");
         XPloder x = new XPloder();
         try {
-            x.xPlode(from, to);
+            x.xPlode(from, to, "TMT");
         } catch (Exception e) {
             System.out.println(e.getMessage());
             e.printStackTrace();

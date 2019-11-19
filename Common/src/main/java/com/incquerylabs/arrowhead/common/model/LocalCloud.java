1e3d2739dc302723c03aa15f0404d8031a1580cd
Koltai Kadosa <kadosa.koltai@gmail.com>
2019-11-19 10:27:52 +0100

    Trying json validation.

--- Common/src/main/java/com/incquerylabs/arrowhead/common/model/LocalCloud.java
+++ Common/src/main/java/com/incquerylabs/arrowhead/common/model/LocalCloud.java
@@ -1,4 +1,9 @@
 package com.incquerylabs.arrowhead.common.model;
 
+import io.vertx.core.json.JsonObject;
+
 public class LocalCloud {
+
+    public LocalCloud(JsonObject cloudJson) {
+    }
 }
--- Common/src/main/java/com/incquerylabs/arrowhead/common/model/Model.java
+++ Common/src/main/java/com/incquerylabs/arrowhead/common/model/Model.java
@@ -1,8 +1,8 @@
 package com.incquerylabs.arrowhead.common.model;
 
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -10,24 +10,35 @@ public class Model {
 
     private Set<LocalCloud> localClouds = new HashSet<>();
 
-    public Model(JsonObject base) {
-
+    public Model(JsonArray base) {
+        String message = null;
+        for (int i = 0; i < base.size(); ++i) {
+            JsonObject cloudJson = base.getJsonObject(i);
+            if (cloudJson != null) {
+                try {
+                    LocalCloud localCloud = new LocalCloud(cloudJson);
+                } catch (IllegalArgumentException e) {
+                    String under = e.getMessage();
+                    message = under + "\n" + message;
+                }
+            } else {
+                message = "Top Array MUST only contain JsonObjects describing Local Clouds!\n" + message;
+            }
+        }
+        if (message != null) {
+            throw new IllegalArgumentException(message);
+        }
     }
 
     public void merge(Model other) {
 
     }
 
-    public JsonObject toJson() {
+    public JsonArray toJson() {
         return null;
     }
 
     public Set<LocalCloud> getLocalClouds() {
         return localClouds;
-    }
-
-    public boolean addLocalCloud(LocalCloud... clouds) {
-        //TODO cascade
-        return localClouds.addAll(Arrays.asList(clouds));
     }
 }
